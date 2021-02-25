ed662a9840eb4e5975aeaab2d75ee8d6fb45c7d8
pengxy <pengxuanyus@gmail.com>
2021-02-25 20:37:17 +0800

    温习~

--- src/main/java/com/pengxy/algorithm/Sort/BubbleSort.java
+++ src/main/java/com/pengxy/algorithm/Sort/BubbleSort.java
@@ -9,11 +9,11 @@
 public class BubbleSort {
     public static void bubbleSort(int[] datas){
         int temp = 0;
-        int size = datas.length;
-        for(int i = 0;i < size - 1 ;i++) {
-            for (int j = 0; j < size - 1 - i; j++) {
+        // int size = datas.length;
+        for(int i = 0;i < datas.length - 1 ;i++) {
+            for (int j = 0; j < datas.length - 1 - i; j++) {
                 if (datas[j] > datas[j + 1]) {
-                    // 如果左边小于右边，交换位置
+                    // 如果左边大于右边，交换位置
                     temp = datas[j];
                     datas[j] = datas[j + 1];
                     datas[j + 1] = temp;
