89ac4a4fddc774c2a1fb1ad086b7edf5a8b843d7
pengxy <pengxuanyus@gmail.com>
2021-02-28 16:35:35 +0800

    leetcode day5

---
+++ src/main/java/com/pengxy/algorithm/Tree/LeetCode/allPath.java
@@ -0,0 +1,34 @@
+package com.pengxy.algorithm.Tree.LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @program algorithm
+ * @description: 所有路径 递归
+ * @author: pengxuanyu
+ * @create: 2021/02/28 16:27
+ */
+public class allPath {
+    List<String> list = new ArrayList<>();
+    public List<String> allPath(TreeNode root){
+
+        if(root == null){
+            return null;
+        }
+        resolve(root , "",list);
+        return list;
+    }
+
+    public void resolve(TreeNode root,String str,List<String> list){
+        if(root == null) return;
+        str += root.val;
+        // 走到底则将节点值放入List中
+        if(root.right == null && root.left == null) {
+            list.add(str);
+        }else{
+            resolve(root.left,str+ "->",list);
+            resolve(root.right,str+"->",list);
+        }
+    }
+}
