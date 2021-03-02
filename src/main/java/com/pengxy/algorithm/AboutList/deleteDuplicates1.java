352a3779f4e04257426ba921fce9bc2f390f321e
pengxy <pengxuanyus@gmail.com>
2021-03-02 22:43:39 +0800

    leetcode day7 复习链表 二叉树

--- src/main/java/com/pengxy/algorithm/AboutList/deleteDuplicates1.java
+++ src/main/java/com/pengxy/algorithm/AboutList/deleteDuplicates1.java
@@ -15,11 +15,16 @@ public class deleteDuplicates1 {
         ListNode fast = head.next;
         while(fast != null){
             if(fast.val != slow.val){
+                // 当快指针的值和慢指针的值不相同时（略过重复值，相当于删除），快慢指针指向的数值是不重复的
+                // slow 的下一个指向fast
+                // slow 所指的数值永远是正确的数值，让fast去试错
                 slow.next = fast;
+                // slow 去到fast的位置  slow++
                 slow = slow.next;
             }
             fast = fast.next;
         }
+        // 断开与后面重复元素的连接，起到删除重复元素的效果
         slow.next = null;
         return head;
     }
