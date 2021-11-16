59a0a3a55077db61aa855f16b299af4017775d65
pengxuanyu <pengxuanyus@gmail.com>
2021-11-16 23:13:15 +0800

    平衡二叉树 + 杨辉三角

--- src/main/java/com/pengxy/algorithm/array/array.java
+++ src/main/java/com/pengxy/algorithm/array/array.java
@@ -5,6 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import apple.laf.JRSUIUtils;
+import com.pengxy.algorithm.Tree.LeetCode.TreeNode;
+
 /**
  * @program algorithm
  * @description:
@@ -105,6 +108,81 @@ public class array {
         int[] temp = new int[digits.length + 1];
         temp[0] = 1;
         return temp;
+    }
+
+    /**
+     * 给你两个按 非递减顺序 排列的整数数组nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+     *
+     * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+     *
+     * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+     * @param nums1
+     * @param m
+     * @param nums2
+     * @param n
+     */
+    public void merge0088(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int p = m + n - 1;
+        // 从后往前排，比num1大的数丢去num1的最后面
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] <= nums2[j]) {
+                nums1[p] = nums2[j];
+                p--;
+                j--;
+            } else {
+                nums1[p] = nums1[i];
+                p--;
+                i--;
+            }
+        }
+        while (j >= 0) {
+            nums1[p] = nums2[j];
+            p--;
+            j--;
+        }
+    }
+
+
+    /**
+     * 给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 高度平衡 二叉搜索树。
+     *
+     * 高度平衡 二叉树是一棵满足「每个节点的左右两个子树的高度差的绝对值不超过 1 」的二叉树。
+     * @param nums
+     * @return
+     */
+    public TreeNode sortedArrayToBST0108(int[] nums) {
+        return this.doSort(nums, 0, nums.length);
+    }
+
+    private TreeNode doSort(int[] nums, int l, int r) {
+        int mid = l + ((r - l) / 2);
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = this.doSort(nums, l, mid);
+        root.right = this.doSort(nums, mid + 1, r);
+        return root;
+    }
+
+    /**
+     * 给定一个非负索引 rowIndex，返回「杨辉三角」的第 rowIndex 行。
+     * @param rowIndex
+     * @return
+     */
+    public List<Integer> getRow0109(int rowIndex) {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        int n = 1;
+        while (n <= rowIndex) {
+            list.add(0);
+            // 我自增之后，遍历我自己
+            for (int i = list.size() - 1; i > 0; i--) {
+                // 杨辉三角的核心
+                list.set(i, list.get(i) + list.get(i - 1));
+            }
+            n++;
+        }
+        return list;
     }
 
 
