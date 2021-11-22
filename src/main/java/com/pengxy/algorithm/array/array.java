f3ea3fc3cb4503d045b6b22cbccf276ca3f90ad8
pengxuanyu <pengxuanyus@gmail.com>
2021-11-22 23:05:47 +0800

    给大佬跪了

--- src/main/java/com/pengxy/algorithm/array/array.java
+++ src/main/java/com/pengxy/algorithm/array/array.java
@@ -339,6 +339,27 @@ public class array {
     }
 
 
+    /**
+     * 给定一个整数数组和一个整数k，判断数组中是否存在两个不同的索引i和j，使得nums [i] = nums [j]，并且 i 和 j的差的 绝对值 至多为 k
+
+     * @param nums
+     * @param k
+     * @return
+     */
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                if (Math.abs(map.get(nums[i]) - i) <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+        return false;
+    }
+
+
 
 
     public static void main(String[] args) {
