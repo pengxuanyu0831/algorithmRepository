6f2cbfc61271174f80d50fefdb2bafdb6b21f194
pengxuanyu <pengxuanyus@gmail.com>
2022-02-04 15:01:13 +0800

    动态规划 day3

--- thinging/src/main/java/DynamicProgramming/DynamicProgramming.java
+++ thinging/src/main/java/DynamicProgramming/DynamicProgramming.java
@@ -165,6 +165,19 @@ public class DynamicProgramming {
     }
 
 
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                // 数量 dp[3] =
+                dp[i] += dp[j - 1] * dp[i - j];
+            }
+        }
+        return dp[n];
+    }
+
+
     public static void main(String[] args) {
         DynamicProgramming dynamicProgramming = new DynamicProgramming();
 
