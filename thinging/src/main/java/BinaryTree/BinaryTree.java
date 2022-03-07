21f94079edea9a372dc928d7a1f1c11e74183555
pengxuanyu <pengxuanyus@gmail.com>
2022-03-07 23:10:45 +0800

    二叉树

---
+++ thinging/src/main/java/BinaryTree/BinaryTree.java
@@ -0,0 +1,13 @@
+package BinaryTree;
+
+/**
+ * @program algorithm
+ * @description:
+ * @author: pengxuanyu
+ * @create: 2022/03/07 22:54
+ */
+public class BinaryTree {
+    /**
+     * 我们把根节点存储在下标 i=1 的位置，它的左子节点存储在下标为 2 * i 的位置，右子节点存储在下标为 2*i+1 的位置。以此类推，B 节点、C 节点的左右子节点都按照这种规律进行存储
+     */
+}
