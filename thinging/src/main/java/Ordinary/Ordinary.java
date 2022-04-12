adc7169e0ad97301ca28605e8afafb4ff48f113c
pengxuanyu <pengxuanyus@gmail.com>
2022-04-12 23:04:39 +0800

    每日一题 day2

---
+++ thinging/src/main/java/Ordinary/Ordinary.java
@@ -0,0 +1,63 @@
+package Ordinary;
+
+import org.apache.logging.log4j.util.Strings;
+
+import java.util.*;
+
+/**
+ * @program algorithm
+ * @description:
+ * @author: pengxuanyu
+ * @create: 2022/04/12 22:04
+ */
+public class Ordinary {
+
+    /**
+     * #806
+     * @param widths
+     * @param s
+     * @return 这勾八题目都看不懂的真的傻逼
+     */
+    public int[] numberOfLines(int[] widths, String s) {
+        char[] chars = s.toCharArray();
+        int target = 0;
+        int row = 1;
+        for (int i = 0; i < chars.length; i++) {
+            // char[i] - 'a' -->>>等价于减去字母a的ASCII码 97
+            target += widths[chars[i] - 'a'];
+            if (target > 100) {
+                row++;
+                target = widths[chars[i] - 'a'];
+            }
+        }
+        return new int[]{row, target};
+    }
+
+
+    /**
+     * #804
+     * @param words
+     * @return
+     */
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] mos = new String[]{".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        String[] abc = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+        Set<String> result = new HashSet<>();
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < mos.length; i++) {
+            map.put(abc[i], mos[i]);
+        }
+        for (String s : words) {
+            char[] c = s.toCharArray();
+            StringBuffer sb = new StringBuffer();
+            for (int j = 0; j < c.length; j++) {
+                System.out.println(c[j] + "");
+                String mosStr = map.get(c[j]+"");
+                sb.append(mosStr);
+                System.out.println(s + "=" + sb);
+            }
+            result.add(sb.toString());
+        }
+        return result.size();
+    }
+}
