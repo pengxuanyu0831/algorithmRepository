60d9b2a9eebe1039375a5ddfa53fc4a925517a58
pengxuanyu <pengxuanyus@gmail.com>
2022-04-20 23:39:59 +0800

    【回溯】回溯算法之初窥门道 day2

--- thinging/src/main/java/BackTracking/BackTracking.java
+++ thinging/src/main/java/BackTracking/BackTracking.java
@@ -1,6 +1,7 @@
 package BackTracking;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -99,12 +100,91 @@ public class BackTracking {
             return;
         }
 
-        for (int i = index; i < candidates.length; i++) {
+        for (int i = index; i < candidates.length && sum + candidates[i] <= target; i++) {
             sum += candidates[i];
             path.add(candidates[i]);
+            // 这里没有传i+1，就代表下次进来可以再次使用自己（重复使用）
+            Arrays.sort(candidates);
             this.combinationSumBackTracking(target, candidates,sum,i);
             sum -= candidates[i];
             path.remove(path.size() - 1);
         }
+    }
+
+
+    /**
+     * #40
+     *
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        this.combinationSum2BackTracking(target, candidates, 0, 0);
+        return result;
+
+    }
+
+    private void combinationSum2BackTracking(int target, int[] candidates,int sum,int index){
+        if (sum > target) {
+            return;
+        }
+
+        if (sum == target) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = index; i < candidates.length && sum + candidates[i] <= target; i++) {
+            // 同一树层使用过的元素，不允许再使用了 看pdf的图就懂了
+            if (i > index && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+            sum += candidates[i];
+            path.add(candidates[i]);
+
+            // 本题不允许重复使用元素
+            this.combinationSum2BackTracking(target, candidates,sum,i + 1);
+            sum -= candidates[i];
+            path.remove(path.size() - 1);
+        }
+    }
+
+
+    /**
+     * #131
+     */
+    List<String> pathString = new ArrayList<>();
+    List<List<String>> resultString = new ArrayList<>();
+    public List<List<String>> partition(String s) {
+        this.doPartition(s, 0);
+        return resultString;
+    }
+
+    public void doPartition(String s, int index) {
+        if (index >= s.length()) {
+            resultString.add(new ArrayList<>(pathString));
+            return;
+        }
+        char[] chars = s.toCharArray();
+        for (int i = index; i < chars.length; i++) {
+            if (isBack(chars, index, i)) {
+                pathString.add(s.substring(index, i+ 1));
+            } else {
+                continue;
+            }
+            doPartition(s, i + 1);
+            pathString.remove(pathString.size() - 1);
+        }
+    }
+
+    boolean isBack(char[] chars, int index, int end) {
+        for (int i = index, j = end; i < j; i++, j--) {
+            if (chars[i] != chars[j]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
