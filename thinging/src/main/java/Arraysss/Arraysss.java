b23aad37bab4818dcf0c42f03d02580b7cf7586f
pengxuanyu <pengxuanyus@gmail.com>
2022-05-30 23:54:27 +0800

    【随想录】第一次跟刷 day1

---
+++ thinging/src/main/java/Arraysss/Arraysss.java
@@ -0,0 +1,53 @@
+package Arraysss;
+
+/**
+ * @program algorithm
+ * @description:
+ * @author: pengxuanyu
+ * @create: 2022/05/30 23:05
+ */
+public class Arraysss {
+    /**
+     * #704
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) / 2);
+            // 中间值大于目标值 -----> 目标值落在了左半区间
+            if (nums[mid] > target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+
+    /**
+     * #27
+     * @param nums
+     * @param val
+     * @return
+     */
+    public int removeElement(int[] nums, int val) {
+        int fast = 0;
+        int slow;
+        for (slow = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+
+    }
+
+}
