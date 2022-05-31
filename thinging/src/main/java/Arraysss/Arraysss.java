9e6fbea10abfca40d94bc6734f19c1b14e370e3a
pengxuanyu <pengxuanyus@gmail.com>
2022-05-31 23:33:37 +0800

    【随想录】第一次跟刷 day2

--- thinging/src/main/java/Arraysss/Arraysss.java
+++ thinging/src/main/java/Arraysss/Arraysss.java
@@ -70,4 +70,50 @@ public class Arraysss {
         return slow + 1;
     }
 
+
+    /**
+     * #283
+     * @param nums
+     */
+    public void moveZeroes(int[] nums) {
+        int slow = 0;
+        int fast = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != 0) {
+                nums[slow] = nums[fast];
+                fast++;
+                slow++;
+            } else {
+                fast++;
+            }
+        }
+        for (int i = slow; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+
+
+    /**
+     * #844
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean backspaceCompare(String s, String t) {
+        int fast = 1;
+        int slow;
+        char[] chars = new char[s.length()];
+        char[] chart = new char[t.length()];
+        for (slow = 0; fast < s.toCharArray().length; fast++) {
+            if ('#' == s.charAt(fast)) {
+                fast++;
+                slow++;
+            } else {
+                chars[fast] = s.charAt(fast);
+                slow++;
+            }
+        }
+
+    }
+
 }
