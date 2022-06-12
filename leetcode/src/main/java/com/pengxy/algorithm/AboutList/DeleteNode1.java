5d171234785c29d55bc1aab66af896bacf4d436c
pengxuanyu <pengxuanyus@gmail.com>
2022-06-13 00:04:53 +0800

    【随想录】第一次跟刷 day8 【链表 day1】

--- leetcode/src/main/java/com/pengxy/algorithm/AboutList/DeleteNode1.java
+++ leetcode/src/main/java/com/pengxy/algorithm/AboutList/DeleteNode1.java
@@ -8,28 +8,16 @@ package com.pengxy.algorithm.AboutList;
  */
 public class DeleteNode1 {
     public static ListNode deletenode(ListNode head, int val) {
-        if (head == null) {
-            return head;
-        }
-        // 需要一个临时节点作为头结点，否则第一个数就是要删的节点的情况，会空指针
-        ListNode tempnode = new ListNode();
-        tempnode.next = head;
-        ListNode pre = tempnode;
-        ListNode cur = head;
-        // 只要节点不为空，就要循环
-        while(cur != null){
-            ListNode temp = cur.next;
-            // 当数值等于给的值时
-            if(cur.val == val){
-                // 前一节点的指针，略过相等的值得指针，指向相等数下一位的地址
-                pre.next = temp;
-                break;
+        ListNode temp = new ListNode();
+        temp.next = head;
+        ListNode cur = temp;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
             }
-            // 要一直循环，往下走，当前变为上一个，下一个变为当前
-            pre = cur;
-            cur = cur.next;
         }
-        // 返回临时节点的下一位，这里是因为临时节点本身就不存在，仅作为一个参照物
-        return tempnode.next;
+        return temp.next;
     }
 }
