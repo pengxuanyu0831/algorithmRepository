f80b60b2ddeee95307d87f43695d4e84796014ee
pengxuanyu <pengxuanyus@gmail.com>
2022-06-12 23:54:53 +0800

    【随想录】第一次跟刷 day8 【链表 day1】

--- leetcode/src/main/java/com/pengxy/algorithm/AboutList/ListNode.java
+++ leetcode/src/main/java/com/pengxy/algorithm/AboutList/ListNode.java
@@ -7,13 +7,17 @@ package com.pengxy.algorithm.AboutList;
  * @create: 2021/02/21 13:37
  */
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
     ListNode(int x){
         val = x;
     }
 
     public ListNode() {
-
+    }
+    // 节点的构造函数(有两个参数)
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
