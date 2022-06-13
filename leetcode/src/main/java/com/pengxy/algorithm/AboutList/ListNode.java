27301ea9b5cdb5cd66608790d9d4771b7a80b8c5
pengxuanyu <pengxuanyus@gmail.com>
2022-06-13 22:56:08 +0800

    【随想录】第一次跟刷 day9 【链表 day2】

--- leetcode/src/main/java/com/pengxy/algorithm/AboutList/ListNode.java
+++ leetcode/src/main/java/com/pengxy/algorithm/AboutList/ListNode.java
@@ -9,7 +9,7 @@ package com.pengxy.algorithm.AboutList;
 public class ListNode {
     public int val;
     public ListNode next;
-    ListNode(int x){
+    public ListNode(int x){
         val = x;
     }
 
--- thinging/src/main/java/List/Listttt.java
+++ thinging/src/main/java/List/Listttt.java
@@ -30,6 +30,74 @@ public class Listttt {
             }
         }
         return temp.next;
+    }
 
+
+    /**
+     * #707
+     */
+    class MyLinkedList {
+        int size;
+
+        ListNode header;
+
+        public MyLinkedList() {
+            size = 0;
+            header = new ListNode(0);
+        }
+
+        MyLinkedList list = new MyLinkedList();
+
+        public int get(int index) {
+            if (index < 0 || index > list.size) {
+                return -1;
+            }
+            ListNode node = header;
+            for (int i = 0; i <= index; i++) {
+                node = node.next;
+            }
+            return node.val;
+        }
+
+        public void addAtHead(int val) {
+            this.addAtIndex(0, val);
+
+        }
+
+        public void addAtTail(int val) {
+            this.addAtIndex(size, val);
+        }
+
+        public void addAtIndex(int index, int val) {
+            if (index > size) {
+                return;
+            }
+            if (index < 0) {
+                index = 0;
+            }
+            // 需要一个虚拟头结点，所以长度+1
+            size += 1;
+            // 找位置
+            ListNode pre = header;
+            for (int i = 0; i < index; i++) {
+                pre = pre.next;
+            }
+            // 创建要添加的节点
+            ListNode node = new ListNode(val);
+            node.next = pre.next;
+            pre.next = node;
+        }
+
+        public void deleteAtIndex(int index) {
+            if (index < 0 || index >= size) {
+                return;
+            }
+            size -= 1;
+            ListNode pre = header;
+            for (int i = 0; i < index; i++) {
+                pre = pre.next;
+            }
+            pre.next = pre.next.next;
+        }
     }
 }
