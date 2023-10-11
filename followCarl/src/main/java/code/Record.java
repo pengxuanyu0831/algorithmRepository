5d2fd6eed3c43927c11405156cde3e1499495ab7
pengxuanyu <pengxuanyus@gmail.com>
2023-10-12 00:13:16 +0800

    [Carl哥算法跟刷]day1 二分

--- followCarl/src/main/java/code/Record.java
+++ followCarl/src/main/java/code/Record.java
@@ -1,10 +1,13 @@
 package code;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author xuanyu peng
  * @description:
  * @date 2023/10/11 23:39
  */
+@Slf4j
 public class Record {
 
 
@@ -16,17 +19,26 @@ public class Record {
      */
     public int search(int[] nums, int target) {
         int left = 0;
-        int right = nums.length - 1;
+        int right = nums.length -1;
         while (left <= right) {
-            int idx = (left + right) / 2;
-            if (target == nums[idx]) {
-                return idx;
-            } else if (target < nums[idx]) {
-                right = idx;
-            } else if ( target >= nums[idx]) {
+            int idx = (left + right) / 2 ;
+
+            if (target < nums[idx]) {
+                right = idx -1  ;
+            } else if (target > nums[idx]) {
                 left = idx + 1;
+            } else {
+                return idx;
             }
         }
         return -1;
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-1,0,3,5,9,12};
+        int target = 9;
+        Record record = new Record();
+        log.info("res >>>{}", record.search(nums, target));
     }
 }
