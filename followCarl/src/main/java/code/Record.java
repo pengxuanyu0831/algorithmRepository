0b161e8d6d74479231b06ebf39ed33c9e36e2691
pengxuanyu <pengxuanyus@gmail.com>
2023-11-26 22:04:43 +0800

    [Carl哥算法跟刷]Day42

--- followCarl/src/main/java/code/Record.java
+++ followCarl/src/main/java/code/Record.java
@@ -1084,6 +1084,35 @@ public class Record {
     }
 
 
+    /**
+     * Day43 #1049
+     * @param stones
+     * @return
+     */
+    public int lastStoneWeightII(int[] stones) {
+        int sum = 0;
+
+        // 加总>>两堆石头互相抵消
+        for (int i : stones) {
+            sum += i;
+        }
+
+        int target = sum / 2;
+
+        int[] dp = new int[target + 1];
+        dp[0] = 0;
+
+        // 从左至右遍历物品
+        for (int i = 0; i < stones.length; i++) {
+            for (int j = target; j >= stones[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - stones[i]] + stones[i]);
+            }
+        }
+
+        return sum - 2 * dp[target];
+    }
+
+
 
     public static void main(String[] args) {
         int target =2 ;
