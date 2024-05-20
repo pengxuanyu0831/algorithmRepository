a941c8c22a93bd0d902101e566e8ef767d212024
pengxuanyu <pengxuanyus@gmail.com>
2024-05-20 23:23:09 +0800

    leetcode热门100题 day3

--- interview/src/main/java/com/xy/LeetCodeHot100.java
+++ interview/src/main/java/com/xy/LeetCodeHot100.java
@@ -80,25 +80,51 @@ public class LeetCodeHot100 {
      * @return
      */
     public int longestConsecutive(int[] nums) {
-        Arrays.sort(nums);
-        if (nums.length == 0) {
-            return 0;
+//        Arrays.sort(nums);
+//        if (nums.length == 0) {
+//            return 0;
+//        }
+//
+//        int res = 1;
+//
+//        int left = 0;
+//        int right = 1;
+//        while (right < nums.length) {
+//            if (nums[right] - nums[left] == 1) {
+//                res += 1;
+//                log.info("left >>[{}]--[{}]  right >>[{}]--[{}] res >> {}",left,nums[left],right,nums[right],res);
+//            }else {
+//                res = 1;
+//            }
+//            left = right;
+//            right += 1;
+//        }
+//        log.info("res >>> {}" , res);
+//        return res;
+
+        Set<Integer> resSet = new HashSet<>();
+
+        for (int i : nums) {
+            resSet.add(i);
         }
 
-        int res = 1;
+        int res = 0;
 
-        int left = 0;
-        int right = 1;
-        while (right < nums.length) {
-            if (nums[right] - nums[left] == 1) {
-                log.info("left >>[{}]--[{}]  right >>[{}]--[{}]",left,nums[left],right,nums[right]);
-                res += 1;
-            }else {
-                res = 1;
+        for (int i : resSet) {
+            // 遍历每个数字，看这个数字-1是否存在，即判断当前数字是否为一组顺序数字的开头，如果不存在，即当前数字是一组顺序的开头
+            if (!resSet.contains(i - 1)) {
+                int currentInt = i;
+                int currentRes = 1;
+
+                while (resSet.contains(currentInt + 1)) {
+                    currentInt += 1;
+                    currentRes += 1;
+                }
+
+                res = Math.max(res, currentRes);
             }
-            left = right;
-            right += 1;
         }
+
         return res;
     }
 
@@ -106,10 +132,10 @@ public class LeetCodeHot100 {
     public static void main(String[] args) {
         LeetCodeHot100 leetCodeHot100 = new LeetCodeHot100();
 
-        int[] ints1 = {9,1,4,7,3,-1,0,5,8,-1,6};
+        int[] ints1 = {9,1,4,7,3,-1,0,5,8,-1,6}; // 3 4 5 6 7 8 9
         int[] ints2 = {100,4,200,1,3,2};
 
-        int longested = leetCodeHot100.longestConsecutive(ints2);
+        int longested = leetCodeHot100.longestConsecutive(ints1);
         System.out.println(longested);
     }
 
